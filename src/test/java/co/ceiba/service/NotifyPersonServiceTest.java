621253fa72e49eb06734c9382f9029dbe989ee65
Edward Alexis Ortiz Agudelo - Ceiba Software <edward.ortiz@ceiba.com.co>
2018-02-20 14:18:43 -0500

    I fixed the test

--- src/test/java/co/ceiba/service/NotifyPersonServiceTest.java
+++ src/test/java/co/ceiba/service/NotifyPersonServiceTest.java
@@ -1,7 +1,6 @@
 package co.ceiba.service;
 
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -27,6 +26,6 @@ public class NotifyPersonServiceTest {
 		// Act
 		String message = notifyPersonService.notify(person);
 		// Assert
-		assertNull(message);
+		assertNotNull(message);
 	}
 }
